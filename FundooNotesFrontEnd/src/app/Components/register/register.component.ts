77d70bcebe0239b11145b00ccb8426e65f7d1e9c
Diwakar <diwakar.n@tvsnext.io>
2021-09-15 17:07:27 +0530

    [Diwakar]Add: Validate password and confirm password

--- FundooNotesFrontEnd/src/app/Components/register/register.component.ts
+++ FundooNotesFrontEnd/src/app/Components/register/register.component.ts
@@ -9,18 +9,38 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   RegisterForm !: FormGroup;
   hide:boolean =  true;
+  isVisible:boolean =  true;
+  matchpwd:boolean = true;
   constructor() { }
 
   ngOnInit(): void {
     this.RegisterForm = new FormGroup({
       firstName: new FormControl('',[Validators.required, Validators.pattern('^[A-Z]{1}[a-zA-Z]{2,}'),Validators.minLength(3)]),
       lastName: new FormControl('',[Validators.required, Validators.pattern('^[A-Z]{1}[a-zA-Z]{2,}'),Validators.minLength(3)]),
-
+      email: new FormControl('',[Validators.required, Validators.email]),
+      password:new FormControl('',[Validators.required, Validators.pattern('^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$'),Validators.minLength(8)]),
+      confirmPassword:new FormControl('',[Validators.required])
     });
   }
+  
   showPassword()
   {
     this.hide = !this.hide;
+  }
+  getErrorMessage(inputName:string) {
+      let minLen = inputName=="password"?8:3;
+      
+      if (this.RegisterForm.controls[`${inputName}`].hasError('required')) {
+        return 'You must enter a value';
+      }
+      else if(this.RegisterForm.controls[`${inputName}`].hasError('minlength')){
+        return `minimum ${minLen} characters`;
+      }
+      else if(this.RegisterForm.controls[`${inputName}`].hasError('email'))
+      {
+        return `${inputName} is invalid`;
+      }
+      return this.RegisterForm.controls[`${inputName}`].hasError('pattern') ? `${inputName} is invalid` : '';
   }
 }
 
