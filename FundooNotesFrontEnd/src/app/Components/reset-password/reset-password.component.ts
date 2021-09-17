5b6cdf9e9f8b651044733425efc964b6cf7072bf
Diwakar <diwakar.n@tvsnext.io>
2021-09-17 11:49:55 +0530

    [Diwakar]Add:validations in reset password page

--- FundooNotesFrontEnd/src/app/Components/reset-password/reset-password.component.ts
+++ FundooNotesFrontEnd/src/app/Components/reset-password/reset-password.component.ts
@@ -9,10 +9,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
-
+  hide:boolean =  true;
+  ResetPasswordForm !: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.ResetPasswordForm = new FormGroup({
+      password:new FormControl('',[Validators.required, Validators.pattern('^.*(?=.{8,})(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$')]),
+      confirmPassword:new FormControl('',[Validators.required])
+    });
   }
-
+  showPassword()
+  {
+    this.hide = !this.hide;
+  }
+  getErrorMessage() {
+    if (this.ResetPasswordForm.controls['password'].hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.ResetPasswordForm.controls['password'].hasError('pattern') ? `password should contain mix of numbers,speacial characters,captial and small letters and minimum 8 characters` : '';
+}
 }
